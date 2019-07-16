0eb67519c0a85faf69c10529c9f12ae0eaf093f6
robert-nicoara <nicoara.robert@gmail.com>
2019-07-16 15:13:12 +0200

    Services to list

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "docker_container" "LocalStackContainer" {
   image = "${docker_image.LocalStack.latest}"
   name  = "localstackcontainer"
 
-  env = ["ports=4567-4584:4567-4584", "SERVICES=${var.services}", "DATA_DIR=${var.data_directory}", "PORT_WEB_UI=8080", "LAMBDA_EXECUTOR=docker", "DOCKER_HOST=unix:///var/run/docker.sock" ]
+  env = ["SERVICES=${var.services}", "ports=4567-4584:4567-4584", "DATA_DIR=${var.data_directory}", "PORT_WEB_UI=8080", "LAMBDA_EXECUTOR=docker", "DOCKER_HOST=unix:///var/run/docker.sock" ]
   
   volumes {
       //volume_name = "${docker_volume.DataHolder.name}"
