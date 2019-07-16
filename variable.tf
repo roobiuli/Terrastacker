dbece0d31e87560f9cfcd3b244aba3cab1e2ee60
robert-nicoara <nicoara.robert@gmail.com>
2019-07-16 15:04:16 +0200

    Services to list

--- variable.tf
+++ variable.tf
@@ -1,8 +1,13 @@
 variable "services" {
-    type = "string"
+    type = "list"
 }
 
 variable "data_directory" {
     type = "string"
     default = "/tmp/localstack/data"
+}
+
+variable "docker_host" {
+    type = "string"
+    default = "unix:///var/run/docker.sock"
 }
